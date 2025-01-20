c6e6cc32b8a056bc8798c79e80a763e946d0620f
The Developer <ianderson.almeida@gmail.com>
2025-01-20 16:26:44 -0300

    Adding backend-side

--- components/Login.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-export function Login(){
-    return(
-        <div>
-          <button className="border-2 border-slate-800  rounded-lg  w-20  hover:bg-slate-100 ">Login</button>
-        </div>
-    )
-}
