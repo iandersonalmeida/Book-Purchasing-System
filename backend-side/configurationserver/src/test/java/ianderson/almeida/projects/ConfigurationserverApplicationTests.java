99c3ece7fb7be96e70fd2a0fd14455220977f018
The Developer <ianderson.almeida@gmail.com>
2025-01-21 15:30:01 -0300

    Adding backend local files

---
+++ backend-side/configurationserver/src/test/java/ianderson/almeida/projects/ConfigurationserverApplicationTests.java
@@ -0,0 +1,13 @@
+package ianderson.almeida.projects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
