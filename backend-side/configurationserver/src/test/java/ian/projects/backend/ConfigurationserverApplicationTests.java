4b67dd46ce6bde2c2650462cd7a5604d4c2af4dc
The Developer <ianderson.almeida@gmail.com>
2025-01-22 14:33:00 -0300

    ,

---
+++ backend-side/configurationserver/src/test/java/ian/projects/backend/ConfigurationserverApplicationTests.java
@@ -0,0 +1,13 @@
+package ian.projects.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
