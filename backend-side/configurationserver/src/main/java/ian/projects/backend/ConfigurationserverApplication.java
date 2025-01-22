01fe865a021333022dc15c78f4e19a46ce97939d
The Developer <ianderson.almeida@gmail.com>
2025-01-22 14:41:27 -0300

    Modified configuration server

--- backend-side/configurationserver/src/main/java/ian/projects/backend/ConfigurationserverApplication.java
+++ backend-side/configurationserver/src/main/java/ian/projects/backend/ConfigurationserverApplication.java
@@ -2,8 +2,10 @@ package ian.projects.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigurationserverApplication {
 
 	public static void main(String[] args) {
