82caae39b14a47c2cebed083277c2c6cfb42f916
The Developer <ianderson.almeida@gmail.com>
2025-01-22 16:01:04 -0300

    New files in frontend-side

---
+++ frontend-side/mountain-vision-web/mountain-vision-app/app/login-page/page.tsx
@@ -0,0 +1,20 @@
+'use client'
+import Link from 'next/link';
+
+
+function Login(){
+    return(       
+        <div>
+           <h1>Login Page</h1>
+           <Link
+              href="/"
+            >
+                Go Back Home
+            </Link>
+
+        </div>
+       
+       )
+}
+
+export default Login;
---
+++ frontend-side/mountain-vision-web/mountain-vision-app/app/search-display/page.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+function SearchDisplay(){
+    return(
+        <div>
+             <h1>Show data here</h1>
+             <Link
+             href="/"
+             >
+                Go Back Home
+             </Link>
+        </div>
+       
+    )
+}
+
+export default SearchDisplay;
